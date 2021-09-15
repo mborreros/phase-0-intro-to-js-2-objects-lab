25abfdd71e4ac2fd3668167b4eb2d7ca5e0c7a27
Maya Borrero <mlbsindia@gmail.com>
2021-09-15 15:57:21 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Maya Borrero",
+    streetAddress: "136 Race Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newName = {...employee};
+    newName [key] = value;
+    return newName
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee [key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const removedName = {...employee};
+    delete removedName.name;
+    return removedName;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+}
